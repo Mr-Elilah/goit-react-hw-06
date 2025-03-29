22b6089ac16fe44a99f22a39551da26d09e8acf6
Mr.Elijah <beztalannyj.ilia@gmail.com>
2025-03-28 21:42:08 -1000

    rename

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { persistor, store } from "./redaxus/store";
+import { persistor, store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import App from "./components/App/App";
 import { store } from "../../redux/store";
