37e63771ba854744a52f27e873557ece37d24968
Mr.Elijah <beztalannyj.ilia@gmail.com>
2025-03-28 21:52:47 -1000

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import App from "./components/App/App";
-import { store } from "../../redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
